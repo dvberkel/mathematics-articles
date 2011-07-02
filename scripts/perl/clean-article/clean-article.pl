7accfc4bf3c2b99eb693f0a343b91031df643009
dvberkel <daan.v.berkel.1980@gmail.com>
2011-07-02 15:28:09 +0000

    Added extensions
    More extensions are added which are generated by latex.

--- scripts/perl/clean-article/clean-article.pl
+++ scripts/perl/clean-article/clean-article.pl
@@ -10,7 +10,7 @@ use Getopt::Long;
 use Cwd;
 
 # The extensions of the files to clean.
-my @clean = qw/log aux dvi ps pdf out nav snm toc blg bbl/;
+my @clean = qw/log aux dvi ps pdf out nav snm toc lof lot glo idx ilg ind blg bbl/;
 
 # Starting point of cleansing operation, defaults to the current working
 # directory.
